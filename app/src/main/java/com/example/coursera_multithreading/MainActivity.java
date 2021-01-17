38feca4b4e17214f065ab4473f33eb8f6cc0e00f
buchgit <buchgit@gmail.com>
2021-01-17 13:01:20 +0300

    HaMeR original from coursera

--- app/src/main/java/com/example/coursera_multithreading/MainActivity.java
+++ app/src/main/java/com/example/coursera_multithreading/MainActivity.java
@@ -1,153 +1,102 @@
 package com.example.coursera_multithreading;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
-import android.util.Log;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.ref.WeakReference;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
+public class MainActivity extends AppCompatActivity implements ImageProcessThread.Callback{
 
-/*
-InputStream так и не заработал
- */
+    //весь код ниже выполняется в мейнтреде
 
-public class MainActivity extends AppCompatActivity {
+    private ImageView mDoge;
 
-    private MyTask myTask;
-    private final String url = "http://i0.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg";
+    private ProgressBar mProgressBar;
+
+    private ImageProcessThread mImageProcessThread;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        Button buttonStart = findViewById(R.id.btn_start);
-        buttonStart.setOnClickListener(new View.OnClickListener() {
+        final Button performBtn = findViewById(R.id.btn_perform);
+
+        mDoge = findViewById(R.id.iv_doge);
+
+        mProgressBar = findViewById(R.id.progress);
+
+        //создаем новый экземпляр фонового потока потока
+
+        mImageProcessThread = new ImageProcessThread("Background ");
+
+        //запускаем поток и инициализируем Looper
+
+        mImageProcessThread.start();
+
+        mImageProcessThread.getLooper();  // -> вызовется onLooperPrepared()
+
+        mImageProcessThread.setCallback(this);
+
+        //теперь в фоновом потоке будет крутиться лупер и ждать задач
+
+
+
+        performBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
+
             public void onClick(View v) {
-                myTask = new MyTask(MainActivity.this);
-                myTask.execute(url);
+
+                //выдергиваем Bitmap из ImageView и скармливаем в наш поток,
+
+                //просто вызывая метод на нем
+
+                BitmapDrawable drawable = (BitmapDrawable) mDoge.getDrawable();
+
+                mImageProcessThread.performOperation(drawable.getBitmap());
+
             }
+
         });
 
-        Button buttonCancel = findViewById(R.id.btn_cancel);
-        buttonCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (myTask!=null){
-                    myTask.cancel(true);
-                }
-            }
-        });
     }
 
-    public ImageView getImageView (){
-        return findViewById(R.id.imv);
+    // методы колбека из интерфейса
+
+    @Override
+
+    public void sendProgress(int progress) {
+
+        mProgressBar.setProgress(progress);
+
     }
 
-    public ProgressBar getProgressBar(){
-        return findViewById(R.id.prb_main_activity);
+    @Override
+
+    public void onCompleted(Bitmap bitmap) {
+
+        mDoge.setImageBitmap(bitmap);
+
     }
 
-    private static class MyTask extends AsyncTask<String,Void, Bitmap> {
+    @Override
 
-        private final String TAG = MyTask.class.getSimpleName()+ " ###== ";
+    protected void onDestroy() {
 
-        WeakReference<MainActivity> mainActivityWeakReference;
+        //гасим фоновый поток, не мусорим
 
-        MyTask(MainActivity activity){
-            mainActivityWeakReference = new WeakReference<>(activity);
-        }
+        mImageProcessThread.quit();
 
-        @Override
-        protected void onPreExecute() {
-            MainActivity activity = (MainActivity) mainActivityWeakReference.get();
-            if (activity!=null){
-                activity.getProgressBar().setVisibility(View.VISIBLE);
-            }
+        super.onDestroy();
 
-        }
-
-//    @Override
-//    protected Bitmap doInBackground (String... strings) {
-//        String url = strings[0];
-//
-//        try {
-//            TimeUnit.SECONDS.sleep(5);
-//            if (isCancelled()){
-//                return null;
-//            }
-//            InputStream inputStream = null;
-//            inputStream = (InputStream) new URL(url).getContent();
-//            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-//            inputStream.close();
-//            return bitmap;
-//        } catch (Exception e) {
-//            return null;
-//        }
-//    }
-
-        @Override
-        protected Bitmap doInBackground(String... strings) {
-            return getBitmap(strings[0]);
-        }
-
-        private Bitmap getBitmap(String url) {
-            //final String url1 = "http://i0.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg";
-            try {
-
-                TimeUnit.SECONDS.sleep(7);
-                if (isCancelled()) {
-                    return null;
-                }
-                Log.d(TAG, "getBitmap: 1 ###==");
-                //InputStream is = (InputStream) new URL(url).getContent();
-                InputStream is = (InputStream) new URL(url).getContent();
-                Log.d(TAG, "getBitmap: 2 ###==");
-                Bitmap d = BitmapFactory.decodeStream(is);
-                is.close();
-
-                return d;
-            } catch (Exception e) {
-                Log.d(TAG, "getBitmap: 3 ###==");
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            MainActivity activity = mainActivityWeakReference.get();
-            if (activity!=null){
-                Log.d(TAG, "onPostExecute: 4 ###==");
-                activity.getImageView().setImageBitmap(bitmap);
-                Toast.makeText(activity,"show image",Toast.LENGTH_SHORT).show();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            MainActivity activity = mainActivityWeakReference.get();
-            if (activity!=null){
-                Log.d(TAG, "onCancelled: 5 ###==");
-                activity.getProgressBar().setVisibility(View.INVISIBLE);
-                Toast.makeText(activity,R.string.cancel,Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 
 }
-
-
-
