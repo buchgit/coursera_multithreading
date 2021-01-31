a4a086b23d3c5a32ed29f6fe119d8a3fd92c6608
buchgit <buchgit@gmail.com>
2021-01-31 18:19:50 +0300

    Bundle works

--- app/src/main/java/com/example/coursera_multithreading/MainActivity.java
+++ app/src/main/java/com/example/coursera_multithreading/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.coursera_multithreading;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 /*
 Сохраняем данные в Bundle, при смене активити достаем из Bundle
+Если компонентам присвоен id (для TextView, кнопок и т.п.), то о сохранении заботиться система, ничего делать не нужно //ЭТО НЕ РАБОТАЕТ
+Если сохранить нужно свои объекты, то придется заботиться самому
+
+ВАЖНО: нужно использовать метод с ОДНИМ параметром
+protected void onSaveInstanceState(@NonNull Bundle outState) {...}
+так как есть похожий метод с ДВУМЯ параметрами
+public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {...}
+, с ним не будет работать сохранение при смене активити (например, повороте)
  */
 
 public class MainActivity extends AppCompatActivity  {
 
     private final String TAG = MainActivity.class.getSimpleName() + " ###== ";
+
+    public final String MY_OBJECT = "my object";
 
     Button buttonPerform;
     Button buttonMessage;
@@ -23,9 +35,14 @@ public class MainActivity extends AppCompatActivity  {
     TextView textView;
     ProgressBar progressBar;
 
+    MyObject myObject;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myObject = new MyObject();
 
         initUI();
 
@@ -33,13 +50,15 @@ public class MainActivity extends AppCompatActivity  {
 
     }
 
-    void onClick(View view){
+    public void onClick(View view){
         switch (view.getId()){
             case R.id.btn_message:
                 Log.d(TAG, "onClick: buttonMessage");
+                textView.setText("onClick: buttonMessage");
                 break;
             case R.id.btn_perform:
                 Log.d(TAG, "onClick: buttonPerform");
+                textView.setText("onClick: buttonPerform");
                 break;
             default:break;
         }
@@ -52,4 +71,28 @@ public class MainActivity extends AppCompatActivity  {
         progressBar = findViewById(R.id.pr_bar);
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(TAG, "1/onSaveInstanceState: ");
+        outState.putSerializable(MY_OBJECT, myObject);
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "2/onSaveInstanceState: ");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d(TAG, "1/onRestoreInstanceState: ");
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState!=null){
+            myObject = (MyObject) savedInstanceState.getSerializable(MY_OBJECT);
+            textView.setText(myObject.TAG);
+            Log.d(TAG, "2/onRestoreInstanceState: ");
+        }
+        Log.d(TAG, "3/onRestoreInstanceState: ");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
 }
---
+++ app/src/main/java/com/example/coursera_multithreading/MyObject.java
@@ -0,0 +1,7 @@
+package com.example.coursera_multithreading;
+
+import java.io.Serializable;
+
+public class MyObject implements Serializable {
+    public static final String TAG = MyObject.class.getSimpleName() + " ###== ";
+}
