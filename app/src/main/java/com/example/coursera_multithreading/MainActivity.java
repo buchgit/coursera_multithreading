7860af8826b1cf046c1cd9310b65517dc3a9177d
buchgit <buchgit@gmail.com>
2021-02-08 13:56:53 +0300

    start shablon

--- app/src/main/java/com/example/coursera_multithreading/MainActivity.java
+++ app/src/main/java/com/example/coursera_multithreading/MainActivity.java
@@ -1,23 +1,10 @@
 package com.example.coursera_multithreading;
 
-import android.widget.ArrayAdapter;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import org.lucasr.twowayview.TwoWayView;
-
-import java.util.ArrayList;
 
 /*
-формирование горизонтального списка
 
-https://guides.codepath.com/android/implementing-a-horizontal-listview-guide
-
-подтянуть библиотеку через gradle
-в app/build.gradle проекта добавить
-dependencies {
-    ...
-    compile 'org.lucasr.twowayview:twowayview:0.1.4'
-}
 
  */
 
@@ -28,15 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> items = new ArrayList<>();
-        items.add("Item 1");
-        items.add("Item 2");
-        items.add("Item 3");
-        items.add("Item 4");
-
-        ArrayAdapter<String> aItems = new ArrayAdapter<>(this, R.layout.my_simple_list_item_1, items);
-        TwoWayView lvTest = findViewById(R.id.lvItems);
-        lvTest.setAdapter(aItems);
 
     }
 
