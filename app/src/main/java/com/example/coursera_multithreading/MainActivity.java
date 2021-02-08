5143b0738be90e35d21a8df648eee19ac125d470
buchgit <buchgit@gmail.com>
2021-02-08 22:31:29 +0300

    RecyclerView добавлен фрагмент

--- app/src/main/java/com/example/coursera_multithreading/MainActivity.java
+++ app/src/main/java/com/example/coursera_multithreading/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 /*
-
+1. Добавить фрагмент
+2. Добавить адаптер
+3. Добавить холдер
+4. Добавить заглушечные данные
 
  */
 
@@ -15,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState==null){ //при повороте фрагмент сохраняется, поэтому создаем только при запуске приложения
+            getSupportFragmentManager().beginTransaction().replace(R.id.fr_main,RecyclerFragment.newInstance()).commit();
+        }
 
     }
 
