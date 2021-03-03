6bd35cfda484754b07bdbd4e507d3a924964998f
buchgit <buchgit@gmail.com>
2021-03-03 21:40:49 +0300

    init

--- app/src/main/java/com/example/coursera_multithreading/MainActivity.java
+++ app/src/main/java/com/example/coursera_multithreading/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.coursera_multithreading;
 import androidx.appcompat.app.AppCompatActivity;
 
 /*
-
+1. В манифесте разрешение, но лучше использовать runtime permitions,
+2. поэтому В build.gradle добавляем Dexter Permissions Library:
+    implementation 'com.karumi:dexter:4.2.0'
+3.
+4.
+5.
  */
 
 public class MainActivity extends AppCompatActivity {
